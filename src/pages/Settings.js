205c345be02f849a985d45a55f2b6bc6e9091f86
Pedro <pedroshalommrn@hotmail.com>
2022-10-08 10:48:40 -0300

    add req 3

---
+++ src/pages/Settings.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from '../trivia.png';
+
+class Settings extends React.Component {
+  render() {
+    return (
+      <div className="App-header">
+        {/* <img src={ logo } className="App-logo" alt="logo" /> */}
+        <h1 data-testid="settings-title">SETTINGS</h1>
+      </div>
+    );
+  }
+}
+export default Settings;
