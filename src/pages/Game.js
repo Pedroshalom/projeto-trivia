c8fe0795091cc302c65ef92e56e3c09c8faea805
Jonas Jeronimo <jonas.jeronimo@live.com>
2022-10-10 16:43:18 -0300

    Recuperando dados do pull

---
+++ src/pages/Game.js
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import Header from './components/Header';
+import Question from './components/Question';
+
+class Game extends Component {
+  render() {
+    const { history } = this.props;
+    return (
+      <>
+        <Header />
+        <Question history={ history } />
+      </>
+    );
+  }
+}
+
+Game.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}.isRequired;
+
+export default Game;
