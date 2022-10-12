e8b8905a0e506986a37d2c82ebe50ac7b6ed0d62
Jonas Jeronimo <jonas.jeronimo@live.com>
2022-10-11 23:06:51 -0300

    Requisito 10 concluido

--- src/redux/action/index.js
+++ src/redux/action/index.js
@@ -4,16 +4,7 @@ export const ADD_USER = 'ADD_USER';
 export const REQUEST_TOKEN = 'REQUEST_TOKEN';
 export const REQUEST_TOKEN_SUCCESS = 'REQUEST_TOKEN_SUCCESS';
 export const REQUEST_TOKEN_FAILURE = 'REQUEST_TOKEN_FAILURE';
-<<<<<<< HEAD
-export const ADD_SCORE = 'ADD_SCORE';
-
-export const addScore = (score) => ({
-  type: ADD_SCORE,
-  score,
-});
-=======
 export const SCORE_POINTS = 'SCORE_POINTS';
->>>>>>> 068ea3b7488b5bdc0a5b2cdcf0bc416ec64f7755
 
 export const addUser = (user) => ({
   type: ADD_USER,
