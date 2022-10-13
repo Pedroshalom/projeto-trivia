b50808bf8aa81d28b48e5afe211cfd0e8ddd2a89
Jonas Jeronimo <jonas.jeronimo@live.com>
2022-10-13 17:53:07 -0300

    Requisito 18 concluido

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,12 +1,34 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class Ranking extends Component {
+class Ranking extends Component {
+  loginButton = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     return (
       <div>
         <p data-testid="ranking-title">Ranking</p>
-      </div>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ this.loginButton }
+        >
+          Login
 
+        </button>
+      </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default connect()(Ranking);
