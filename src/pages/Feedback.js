5cc741e7650616ef2bfa5f924135109ec15c6727
Pedro <pedroshalommrn@hotmail.com>
2022-10-13 15:10:14 -0300

    add requisito 15

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import propTypes from 'prop-types';
 
 class Feedback extends Component {
+  playAgainClick = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     return (
-      <div data-testid="feedback-text">Feedback</div>
+      <div>
+        <div data-testid="feedback-text">Feedback</div>
+        <button
+          data-testid="btn-play-again"
+          type="button"
+          name="btnPlayAgain"
+          onClick={ this.playAgainClick }
+        >
+          Play Again
+        </button>
+      </div>
     );
   }
 }
+
+Feedback.propTypes = {
+  history: propTypes.shape().isRequired, // req 15
+};
 
 export default Feedback;
